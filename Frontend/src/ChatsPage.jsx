2352615711b048febdb6f4a9aa17c5a1b0017e8a
rahulbatra-19 <batrarahul409@gmail.com>
2023-09-06 23:33:41 +0530

    Added react-chat-engine-advanced for better ui

--- Frontend/src/ChatsPage.jsx
+++ Frontend/src/ChatsPage.jsx
@@ -1,5 +1,15 @@
-const ChatsPage = () => {
-    return <div className="background">Chats..</div>
+import { MultiChatSocket , MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '7df2ccc7-6a57-42a3-970a-94f5dcdb6232',
+        props.user.username,
+        props.user.secret);
+
+    return (<div style={{ height: '100vh' }}>
+        <MultiChatWindow {...chatProps} style={{height: '100%'}} />
+        <MultiChatSocket {...chatProps} />
+
+    </div>)
 }
 
 export default ChatsPage;
